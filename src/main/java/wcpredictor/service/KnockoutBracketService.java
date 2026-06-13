4fe9249b21be7fdd3e2d877ce1d6d94ae0cd82d8
navonod <navonod@github.com>
2026-06-13 14:34:38 +0200

    Add missing RoundType import

--- src/main/java/wcpredictor/service/KnockoutBracketService.java
+++ src/main/java/wcpredictor/service/KnockoutBracketService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import wcpredictor.entity.Match;
+import wcpredictor.entity.RoundType;
 import wcpredictor.entity.Team;
 import wcpredictor.entity.TournamentTeam;
 import wcpredictor.repository.MatchRepository;
